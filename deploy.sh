9154b2f15bae47df1588bbb2fd626c50a020ac26
Felipe <felipealcantarbv@gmail.com>
2021-05-20 13:49:50 -0500

    commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+git init
+
+git add ~/FrontendChallenges/PreviewCardComponent/stats-preview-card-component-main
+git commit -m "commit"
+git branch -M main
+git remote add origin https://github.com/Fel-Bv/FrontendMentorStatsPreviewCardChallenge.git
+git push -u origin main
